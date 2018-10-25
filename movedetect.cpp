ba8a33dbbabe4b1ef0f4787fed802493f1ae9e8b
Blissmode <krithickrockz@gmail.com>
2018-10-26 02:27:21 +0530

    Complete Attack Matrix
    The Attack matrix comprises of all the points under attack by the opponent and also contains the magnitude of attack strength at a time. This can be further used to predict check and the next feasible move.

--- movedetect.cpp
+++ movedetect.cpp
@@ -10,30 +10,460 @@ struct square
 };
 
 square board[8][8];
+int attackMatrix[8][8];
+int moveOf;
+
+void findAttack();
+void setKing(int,int);
+void setQueen(int,int);
+void setRook(int,int);
+void setKnight(int,int);
+void setBishop(int,int);
+void setPawn(int,int);
 
 int main()
 {
-    string s;
+    char s[10];
     for(int i=0;i<8;i++)
     {
         for(int j=0;j<8;j++)
         {
+            attackMatrix[i][j]=0;
             cin>>board[i][j].state;
-            if(board[i])>>board[i][j].piece;
+            if(board[i][j].state!=-1)
+            cin>>board[i][j].piece;
+            else
+            board[i][j].piece='-';
         }
     }
 
-    cout<<"The stored chess board "
+    cout<<"The stored chess board "<<endl;
     for(int i=0;i<8;i++)
     {
         for(int j=0;j<8;j++)
         {
-            if(board[i][j].state==0)
+            if(board[i][j].state==1)
             cout<<"w";
-            else if(board[i][j]==1)
+            else if(board[i][j].state==0)
             cout<<"b";
             cout<<board[i][j].piece<<" ";
         }
         cout<<endl;
     }
+    cin>>s;
+    moveOf=strcmp(s,"white")?0:1;
+    if(moveOf)
+    cout<<"White to play"<<endl;
+    else
+    cout<<"Black to play"<<endl;
+
+    findAttack();
 }
+
+
+
+
+
+void findAttack()
+{
+    for(int i=0;i<8;i++)
+    {
+        for(int j=0;j<8;j++)
+        {
+            if(board[i][j].state==(!moveOf))
+            {
+                switch(board[i][j].piece)
+                {
+                    case 'k':
+                    setKing(i,j);
+                    break;
+                    case 'q':
+                    setQueen(i,j);
+                    break;
+                    case 'r':
+                    setRook(i,j);
+                    break;
+                    case 'n':
+                    setKnight(i,j);
+                    break;
+                    case 'b':
+                    setBishop(i,j);
+                    break;
+                    case 'p':
+                    setPawn(i,j);
+                }
+            }
+        }
+    }
+
+    //Display the attack array
+
+    for(int i=0;i<8;i++)
+    {
+        for(int j=0;j<8;j++)
+        {
+            cout<<attackMatrix[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+
+
+
+void setKing(int x,int y)
+{
+    int i,j;
+    i=x-1;
+    j=y;
+    //north
+    if(i>=0)
+    {
+        if(board[i][j].state==(moveOf) || board[i][j].state==-1)
+        attackMatrix[i][j]++;
+    }
+
+    i=x-1;
+    j=y-1;
+    //north west
+    if(i>=0 && j>=0)
+    {
+        if(board[i][j].state==(moveOf) || board[i][j].state==-1)
+        attackMatrix[i][j]++;
+    }
+
+    i=x;
+    j=y-1;
+    //west
+    if(j>=0)
+    {
+        if(board[i][j].state==(moveOf) || board[i][j].state==-1)
+        attackMatrix[i][j]++;
+    }
+
+    i=x+1;
+    j=y-1;
+    //south west
+    if(i<8 && j>=0)
+    {
+        if(board[i][j].state==(moveOf) || board[i][j].state==-1)
+        attackMatrix[i][j]++;
+    }
+
+    i=x+1;
+    j=y;
+    //south
+    if(i<8)
+    {
+        if(board[i][j].state==(moveOf) || board[i][j].state==-1)
+        attackMatrix[i][j]++;
+    }
+
+    i=x+1;
+    j=y+1;
+    //south east
+    if(i<8 && j<8)
+    {
+        if(board[i][j].state==(moveOf) || board[i][j].state==-1)
+        attackMatrix[i][j]++;
+    }
+
+    i=x;
+    j=y+1;
+    //east
+    if(j<8)
+    {
+        if(board[i][j].state==(moveOf) || board[i][j].state==-1)
+        attackMatrix[i][j]++;
+    }
+
+    i=x-1;
+    j=y+1;
+    //north east
+    if(i>=0 && j<8)
+    {
+        if(board[i][j].state==(moveOf) || board[i][j].state==-1)
+        attackMatrix[i][j]++;
+    }
+    
+}
+
+
+
+void setQueen(int x,int y)
+{
+    setRook(x,y);
+    setBishop(x,y);
+}
+
+
+
+void setRook(int x,int y)
+{
+     int i,j;
+
+    //up
+    i=x-1;
+    j=y;
+    while(i>=0)
+    {
+        if(board[i][j].state==(moveOf))
+        {
+            attackMatrix[i][j]++;
+            break;
+        }
+        else if(board[i][j].state==(!moveOf))
+        break;
+        else
+        attackMatrix[i][j]++;
+        i--;
+    }
+
+    //left
+    i=x;
+    j=y-1;
+    while(j>=0)
+    {
+        if(board[i][j].state==(moveOf))
+        {
+            attackMatrix[i][j]++;
+            break;
+        }
+        else if(board[i][j].state==(!moveOf))
+        break;
+        else
+        attackMatrix[i][j]++;
+        j--;
+    }
+
+    //bottom
+    i=x+1;
+    j=y;
+    while(i<8)
+    {
+        if(board[i][j].state==(moveOf))
+        {
+            attackMatrix[i][j]++;
+            break;
+        }
+        else if(board[i][j].state==(!moveOf))
+        break;
+        else
+        attackMatrix[i][j]++;
+        i++;
+    }
+
+    //right
+    i=x;
+    j=y+1;
+    while(j<8)
+    {
+        if(board[i][j].state==(moveOf))
+        {
+            attackMatrix[i][j]++;
+            break;
+        }
+        else if(board[i][j].state==(!moveOf))
+        break;
+        else
+        attackMatrix[i][j]++;
+        j++;
+    }   
+}
+
+
+
+void setKnight(int x,int y)
+{
+    int i,j;
+    i=x-2;
+    j=y+1;
+    //minus plus
+    if(i>=0 && j<8)
+    {
+        if(board[i][j].state==(moveOf) || board[i][j].state==-1)
+        attackMatrix[i][j]++;
+    }
+
+    i=x-1;
+    j=y+2;
+    if(i>=0 && j<8)
+    {
+        if(board[i][j].state==(moveOf) || board[i][j].state==-1)
+        attackMatrix[i][j]++;
+    }
+
+    i=x+1;
+    j=y+2;
+    //plus plus
+    if(i<8 && j<8)
+    {
+        if(board[i][j].state==(moveOf) || board[i][j].state==-1)
+        attackMatrix[i][j]++;
+    }
+
+    i=x+2;
+    j=y+1;
+    if(i<8 && j<8)
+    {
+        if(board[i][j].state==(moveOf) || board[i][j].state==-1)
+        attackMatrix[i][j]++;
+    }
+
+    i=x+1;
+    j=y-2;
+    //plus minus
+    if(i<8 && j>=0)
+    {
+        if(board[i][j].state==(moveOf) || board[i][j].state==-1)
+        attackMatrix[i][j]++;
+    }
+
+    i=x+2;
+    j=y-1;
+    if(i<8 && j>=0)
+    {
+        if(board[i][j].state==(moveOf) || board[i][j].state==-1)
+        attackMatrix[i][j]++;
+    }
+
+    i=x-1;
+    j=y-2;
+    //minus minus
+    if(i>=0 && j>=0)
+    {
+        if(board[i][j].state==(moveOf) || board[i][j].state==-1)
+        attackMatrix[i][j]++;
+    }
+
+    i=x-2;
+    j=y-1;
+    if(i>=0 && j>=0)
+    {
+        if(board[i][j].state==(moveOf) || board[i][j].state==-1)
+        attackMatrix[i][j]++;
+    }
+}
+
+
+
+void setBishop(int x,int y)
+{
+    int i,j;
+
+    //upper right
+    i=x-1;
+    j=y+1;
+    while(j<8 && i>=0)
+    {
+        if(board[i][j].state==(moveOf))
+        {
+            attackMatrix[i][j]++;
+            break;
+        }
+        else if(board[i][j].state==(!moveOf))
+        break;
+        else
+        attackMatrix[i][j]++;
+        i--;
+        j++;
+    }
+
+    //upper left
+    i=x-1;
+    j=y-1;
+    while(i>=0 && j>=0)
+    {
+        if(board[i][j].state==(moveOf))
+        {
+            attackMatrix[i][j]++;
+            break;
+        }
+        else if(board[i][j].state==(!moveOf))
+        break;
+        else
+        attackMatrix[i][j]++;
+        i--;
+        j--;
+    }
+
+    //bottom right
+    i=x+1;
+    j=y+1;
+    while(i<8 && j<8)
+    {
+        if(board[i][j].state==(moveOf))
+        {
+            attackMatrix[i][j]++;
+            break;
+        }
+        else if(board[i][j].state==(!moveOf))
+        break;
+        else
+        attackMatrix[i][j]++;
+        i++;
+        j++;
+    }
+
+    //bottom left
+    i=x+1;
+    j=y-1;
+    while(i<8 && j>=0)
+    {
+        if(board[i][j].state==(moveOf))
+        {
+            attackMatrix[i][j]++;
+            break;
+        }
+        else if(board[i][j].state==(!moveOf))
+        break;
+        else
+        attackMatrix[i][j]++;
+        i++;
+        j--;
+    }
+    
+}
+
+void setPawn(int x,int y)
+{
+    int i,j;
+
+    if(moveOf)
+    {   
+        i=x+1;
+        j=y-1;
+        if(i<8 && j>=0)
+        {
+            if(board[i][j].state==(moveOf) || board[i][j].state==-1)
+            attackMatrix[i][j]++;
+        }
+
+        i=x+1;
+        j=y+1;
+        if(i<8 && j<8)
+        {
+            if(board[i][j].state==(moveOf) || board[i][j].state==-1)
+            attackMatrix[i][j]++;
+        }
+    }
+    else
+    {
+        i=x-1;
+        j=y-1;
+        if(i>=0 && j>=0)
+        {
+            if(board[i][j].state==(moveOf) || board[i][j].state==-1)
+            attackMatrix[i][j]++;
+        }
+
+        i=x-1;
+        j=y+1;
+        if(i>=0 && j<8)
+        {
+            if(board[i][j].state==(moveOf) || board[i][j].state==-1)
+            attackMatrix[i][j]++;
+        }
+    }
+}
+
